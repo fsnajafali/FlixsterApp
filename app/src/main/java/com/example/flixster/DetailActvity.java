39ff1d7b20a52fa662dc1890b370cb197409fa2a
fsnajafali <fsnajafali@gmail.com>
2020-09-28 13:46:45 -0400

    Added detail actvity

---
+++ app/src/main/java/com/example/flixster/DetailActvity.java
@@ -0,0 +1,18 @@
+package com.example.flixster;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailActvity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        String title = getIntent().getStringExtra("title");
+    }
+}
